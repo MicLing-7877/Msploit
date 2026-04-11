ba459770bbc3a71c9727312e1f5d27723a38c552
MicLing-7877 <1549528531@qq.com>
2026-04-12 07:37:51 +0800

    Add files via upload

---
+++ load/Msploit_Load.lua
@@ -0,0 +1,23 @@
+local startTime = tick()
+
+print([[
+
+███╗   ███╗  ███████╗  ██████╗  ██╗      ██████╗  ██╗  ████████╗
+████╗ ████║  ██╔════╝  ██╔══██╗ ██║     ██╔═══██╗ ██║  ╚══██╔══╝
+██╔████╔██║  ███████╗  ██████╔╝ ██║     ██║   ██║ ██║     ██║   
+██║╚██╔╝██║  ╚════██║  ██╔═══╝  ██║     ██║   ██║ ██║     ██║   
+██║ ╚═╝ ██║  ███████║  ██║      ███████╗╚██████╔╝ ██║     ██║   
+╚═╝     ╚═╝  ╚══════╝  ╚═╝      ╚══════╝ ╚═════╝  ╚═╝     ╚═╝   
+
+]])
+print(string.format("Loaded: %.3f seconds", tick() - startTime))
+
+print("MicLing - owner")
+print("Ccat - Developer")
+print("du8 - Developer")
+print("YuXingChen - Developer")
+print("Abyass-king - Developer")
+print("Anti - Developer")
+print("----------------------------------------------------------------------------")
+print("Welcome to use Msploit.")
+print("Have Fun!!!!")
